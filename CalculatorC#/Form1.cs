00a34ef16b35d92747465f546f4feeb6ca12bcfb
HOKAN <HOKAN@DESKTOP-T0FH254>
2024-04-03 21:32:37 +0300

    Доделан инженерный режим и другое

--- CalculatorC#/Form1.cs
+++ CalculatorC#/Form1.cs
@@ -135,5 +135,144 @@ namespace CalculatorC_
             sq = Math.Sqrt(sq);
             textBox1.Text = System.Convert.ToString(sq);
         }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            double qSinh = Double.Parse(textBox1.Text);
+            qSinh = Math.Sinh(qSinh);
+            textBox1.Text = System.Convert.ToString(qSinh);
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            double qSin = Double.Parse(textBox1.Text);
+            qSin = Math.Sin(qSin);
+            textBox1.Text = System.Convert.ToString(qSin);
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            double qCosh = Double.Parse(textBox1.Text);
+            qCosh = Math.Cosh(qCosh);
+            textBox1.Text = System.Convert.ToString(qCosh);
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            double qCos = Double.Parse(textBox1.Text);
+            qCos = Math.Cos(qCos);
+            textBox1.Text = System.Convert.ToString(qCos);
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            double qTanh = Double.Parse(textBox1.Text);
+            qTanh = Math.Tanh(qTanh);
+            textBox1.Text = System.Convert.ToString(qTanh);
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            double qTan = Double.Parse(textBox1.Text);
+            qTan = Math.Tan(qTan);
+            textBox1.Text = System.Convert.ToString(qTan);
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            textBox1.Text = System.Convert.ToString(a, 2);
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            textBox1.Text = System.Convert.ToString(a, 16);
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            textBox1.Text = System.Convert.ToString(a, 8);
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            textBox1.Text = System.Convert.ToString(a);
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            double a;
+            a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox1.Text);
+            textBox1.Text = System.Convert.ToString(a);
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            double a;
+            a = Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox1.Text);
+            textBox1.Text = System.Convert.ToString(a);
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            double a;
+            a = Convert.ToDouble(1.0 /  Convert.ToDouble(textBox1.Text));
+            textBox1.Text = System.Convert.ToString(a);
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            double ilog = Double.Parse(textBox1.Text);
+            ilog = Math.Log(ilog);
+            textBox1.Text = System.Convert.ToString(ilog);
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            double a;
+            a = Convert.ToDouble(textBox1.Text) / Convert.ToDouble(100);
+            textBox1.Text = System.Convert.ToString(a);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            int a;
+            a = Convert.ToInt32(txtMultiply.Text);
+            for (int i = 1; i < 13; i ++)
+            {
+                istMultiply.Items.Add(i + " x " + a + " = " + a * i);
+            }
+        }
+
+        private void умножениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Width = 864;
+            textBox1.Width = 513;
+            istMultiply.Focus(); 
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            istMultiply.Items.Clear();
+            txtMultiply.Clear();
+        }
     }
 }
